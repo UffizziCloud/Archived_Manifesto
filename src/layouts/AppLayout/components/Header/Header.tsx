9131a3ee7658537b2591b888e6a4201f98722ac9
Alexander Gaponenko <allgapone@gmail.com>
2021-12-03 14:51:03 +0300

    fixed docker, packages, externalLinks

--- src/layouts/AppLayout/components/Header/Header.tsx
+++ src/layouts/AppLayout/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import { AppBar, Toolbar, Link } from '@material-ui/core';
 import clsx from 'clsx';
 import { FC } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link as RouterLink } from 'react-router-dom';
 
 import { appRoutes } from 'routes/appRoutes';
+import externalLinks from 'routes/externalLinks';
 
 import githubLogo from 'assets/githubLogo.svg';
 
@@ -24,14 +25,15 @@ const Header: FC = () => {
         <div className={classes.links}>
           {appRoutes.map(({ path, name }) => (
             <Link
+              component={RouterLink}
               key={path}
               className={clsx(classes.link, isMatchPath({ path, exact: true }) && classes.linkActive)}
-              href={path}
+              to={path}
             >
               {name}
             </Link>
           ))}
-          <Link className={clsx(classes.link, classes.linkButton)} href="https://google.com">
+          <Link className={clsx(classes.link, classes.linkButton)} href={externalLinks.uffizziGitHub}>
             <img src={githubLogo} alt="github logo" className={classes.btnLogo} />
             Contribute
           </Link>
